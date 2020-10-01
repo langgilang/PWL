935426bb0e55b8ff91fbee891cb9e9ebeec25e58
langgilang <gilangsafira1416@gmail.com>
2020-10-01 18:13:35 +0700

    Jobsheet 2 : Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+Route::get('/about', function () {
+    return 'NIM : 1931710154, NAMA : GILANG SURYA PRATAMA';
+});
+
+Route::get('/article/1', function () {
+    return 'Halaman artikel dengan id 1';
+});
+
+Route::get('/article/2', function () {
+    return 'Halaman artikel dengan id 2';
+});
+
